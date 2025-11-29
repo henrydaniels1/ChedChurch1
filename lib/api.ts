86bc306ef4bae48065655b763bec93b5dbed7605
henrydaniels1 <henrydanielchibuzor@gmail.com>
2025-11-29 16:27:47 +0100

    made .env.local changes2

--- lib/api.ts
+++ lib/api.ts
@@ -1,3 +1,4 @@
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 
+  (typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000')
 
 export const apiUrl = (path: string) => `${BASE_URL}${path}`
